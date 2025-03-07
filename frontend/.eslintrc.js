88a020ce9f801b17f2ade3ddf1daf7705da24d66
rykyemm <thib.clauss@gmail.com>
2025-03-07 16:15:07 +0100

    maj et ajout de eslintrc

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': 'off', // Désactiver prop-types car nous utilisons TypeScript
+    'react/react-in-jsx-scope': 'off', // React 17+ n'a pas besoin d'importer React
+    'no-unused-vars': 'warn', // Avertissement au lieu d'erreur pour les variables non utilisées
+    'react/no-unescaped-entities': 'off', // Permettre les apostrophes non échappées
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-undef': 'off', // TypeScript gère déjà cela
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['build/', 'node_modules/', '*.config.js'],
+}; 
