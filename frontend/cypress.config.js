180d87fbfc4efa24a0c008fac1b3bf2f920d5542
rykyemm <thib.clauss@gmail.com>
2025-04-03 09:53:58 +0200

    Enhance Cypress configuration by adding project ID and updating CI/CD workflow to utilize Cypress record key for improved test execution.

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  projectId: '955myx',
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.js',
     video: true,
-    screenshotOnRunFailure: true
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   }
 } 
