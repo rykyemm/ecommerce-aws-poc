2dd26a60ae088754a01dcda5c936333fbdeb8296
rykyemm <thib.clauss@gmail.com>
2025-04-03 10:03:05 +0200

    Update homepage test to check for the existence and content of the h2 element instead of h1

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -1,6 +1,7 @@
 describe('Homepage', () => {
   it('should load the homepage', () => {
     cy.visit('/');
-    cy.get('h1').should('exist');
+    cy.get('h2').should('exist');
+    cy.get('h2').should('contain', 'Produits en vedette');
   });
 }); 
